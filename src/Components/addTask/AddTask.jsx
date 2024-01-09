4a9171c365a46a6c84f26dea920871a2c456341d
Aajugiya Ravi <aajugiyaravi@gmail.com>
2024-01-10 01:44:16 +0530

    Changes in ui

--- src/Components/addTask/AddTask.jsx
+++ src/Components/addTask/AddTask.jsx
@@ -1,50 +1,14 @@
-import React, { useState } from "react";
-import Icon from "@mdi/react";
-import { mdiPlus } from "@mdi/js";
-import { useDispatch } from "react-redux";
+import Icon from '@mdi/react';
+import { mdiOrderBoolAscendingVariant } from '@mdi/js';
+import React from 'react'
 
 function AddTask() {
-
-  const [taskName, setTaskName] = useState('');
-  const [priority, setPriority] = useState('');
-  const [first, setFirst] = useState(second)
-
-  const dispatch = useDispatch();
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    const todo = {
-      title: task.value,
-      description: description.value,
-      priority: priority.value,
-      date: date.value,
-    };    
-    console.log(todo);
-  }
-  
   return (
-    <>
-      <div className="addTask">
-        <div className="addTask-btn">
-          <Icon path={mdiPlus} size={1} />
-          <span>Add Task</span>
-        </div>
-        <div className="add-container">
-          <form onSubmit={(e) => handleSubmit(e)}>
-            <input name="task" type="text" placeholder="Task Name" />
-            <input name="description" type="text" placeholder="Description" />
-            <select name="priority" id="">
-              <option value="1">High</option>
-              <option value="2">Medium</option>
-              <option value="3">Low</option>
-            </select>
-            <input name="date" type="date" />
-            <input type="submit" />
-          </form>
-        </div>
-      </div>
-    </>
-  );
+    <div>
+
+      <Icon path={mdiOrderBoolAscendingVariant} size={2} />
+    </div>
+  )
 }
 
-export default AddTask;
+export default AddTask
--- src/Components/home/Home.jsx
+++ src/Components/home/Home.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import Todo from "./Todo";
 import { useSelector } from "react-redux";
 import AddTask from "../addTask/AddTask";
+import Icon from '@mdi/react';
+import { mdiOrderBoolAscendingVariant } from '@mdi/js';
+
 
 function Home() {
   const todos = useSelector((state) => state.todos);
@@ -16,6 +19,7 @@ function Home() {
           <li>Pending</li>
         </ul>
       </div>
+<Icon path={mdiOrderBoolAscendingVariant} size={1} />
 
       {todos.map((todo) => (
         <Todo id={todo.id} title={todo.title} date={todo.date} label={todo.label}/>
